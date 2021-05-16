388375057af990c63dd7668a11e3d35c31f62b8e
mskib77 <mskib77@gmail.com>
2021-05-16 19:00:45 +0200

    success - dragging labels - setting up entire words

--- tests/tests_on_main_activity.py
+++ tests/tests_on_main_activity.py
@@ -65,16 +65,70 @@ class MainActivityTest(BaseTest):
         ma.get_ordered_list_of_shown_labels(word)
         sleep(1)
 
+    @unittest.skip
     def test_draging_first_letter(self):
         ma = self.ma
         word = ma.get_nazwa_field().text
         polozenie = ma.get_nazwa_field()
         l_list = ma.get_ordered_list_of_shown_labels(word)
+        (x, y) = TestUtils.get_screen_dimensions(self.driver)
+        x = x/2
         # label to drag:
-        ltd = self.driver.find_element_by_id(l_list[0])
+        ltd1 = self.driver.find_element_by_id(l_list[1])
+
+        lObszar = self.driver.find_element_by_id("autyzmsoft.pl.literowiec:id/l_Obszar")
+        xl = lObszar.get_attribute('bounds')
+        print(xl)
 
         action = TouchAction(self.driver);
         # action.long_press(ltd).wait(200).move_to(polozenie).perform().release() - to dziala!
-        action.long_press(ltd).wait(200).move_to(x=200, y=200).perform().release()
+        # action.long_press(ltd).wait(200).move_to(x=200, y=200).perform().release()
+        # action.long_press(ltd).wait(200).move_to(lObszar).perform().release()
+        action.long_press(ltd1).wait(200).move_to(x=55, y=730).perform().release()
+
+        # get rid of actions so the object can be reused
+        self.driver._actions = []
+
+        sleep(1)
+        ltd = self.driver.find_element_by_id(l_list[0])
+        print(f"ltd1: {ltd}, {ltd.text}")
+        action.long_press(ltd).wait(200).move_to(x=150, y=730).perform().release()
+
         sleep(4)
         # action.longPress(elem1).waitAction(3000).moveTo(elem2).perform().release();
+
+    @unittest.skip
+    def test_test_draguj2(self):
+        ma = self.ma
+        word = ma.get_nazwa_field().text
+        l_list = ma.get_ordered_list_of_shown_labels(word)
+        # element to drag:
+        ltd = self.driver.find_element_by_id(l_list[0])
+        action = TouchAction(self.driver)
+        action.long_press(ltd).wait(200).move_to(x=100, y=740).perform().release()
+        sleep(1)
+        ltd = self.driver.find_element_by_id(l_list[1])
+        action = TouchAction(self.driver)
+        action.long_press(ltd).wait(200).move_to(x=150, y=740).perform().release()
+        sleep(1)
+
+    def test_build_the_word(self):
+        ma = self.ma
+        word = ma.get_nazwa_field().text
+        l_list = ma.get_ordered_list_of_shown_labels(word)
+        x_curr = -50
+        dx = 150
+        y0 = 740
+        for id_el in l_list:
+            # ltd - label to drag
+            ltd = self.driver.find_element_by_id(id_el)
+            action = TouchAction(self.driver)
+            x_curr += dx
+            # action.long_press(ltd).wait(200).move_to(x=x_curr, y=y0).perform().release()
+            action.press(ltd).wait(200).move_to(x=x_curr, y=y0).perform().release()
+            sleep(0.2)
+        sleep(15)
+
+
+
+
